7050516e7f2c5c95b9d0394f838b2a8d3a2bc9f3
kotov.a <kotov.a@digdes.com>
2017-05-11 10:17:40 +0300

    WebApi проект

---
+++ Dropbox.WebApi/Controllers/FilesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Dropbox.DataAccess;
+using Dropbox.DataAccess.Sql;
+using File = Dropbox.Model.File;
+
+namespace Dropbox.WebApi.Controllers
+{
+	public class FilesController : ApiController
+	{
+		private const string ConnectionString = "Data Source=106SV0013.digdes.com;Initial Catalog=dropbox;Integrated Security=False;User ID=sa; Password=";
+		private readonly IUsersRepository _usersRepository = new UsersRepository(ConnectionString);
+		private readonly IFilesRepository _filesRepository;
+
+		public FilesController()
+		{
+			_filesRepository = new FilesRepository(ConnectionString, _usersRepository);
+		}
+
+		[HttpPost]
+		public File CreateFile(File file)
+		{
+			return _filesRepository.Add(file);
+		}
+
+		[HttpPut]
+		[Route("api/files/{id}/content")]
+		public async Task UpdateFileContent(Guid id)
+		{
+			var bytes = await Request.Content.ReadAsByteArrayAsync();
+			_filesRepository.UpdateContent(id, bytes);
+		}
+	}
+}
---
+++ Dropbox.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Dropbox.DataAccess;
+using Dropbox.DataAccess.Sql;
+using Dropbox.Model;
+
+namespace Dropbox.WebApi.Controllers
+{
+	public class UsersController : ApiController
+	{
+		private const string ConnectionString = "Data Source=106SV0013.digdes.com;Initial Catalog=dropbox;Integrated Security=False;User ID=sa; Password=";
+		private readonly IUsersRepository _usersRepository = new UsersRepository(ConnectionString);
+		private readonly IFilesRepository _filesRepository;
+
+		public UsersController()
+		{
+			_filesRepository = new FilesRepository(ConnectionString, _usersRepository);
+		}
+
+		/// <summary>
+		/// Create s
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		[HttpPost]
+		public User CreateUser([FromBody]User user)
+		{
+			return _usersRepository.Add(user.Name, user.Email);
+		}
+
+		[HttpGet]
+		public User GetUser(Guid id)
+		{
+			return _usersRepository.Get(id);
+		}
+
+		[HttpDelete]
+		public void DeleteUser(Guid id)
+		{
+			_usersRepository.Delete(id);
+		}
+
+		[Route("api/users/{id}/files")]
+		[HttpGet]
+		public IEnumerable<File> GetUserFiles(Guid id)
+		{
+			return _filesRepository.GetUserFiles(id);
+		}
+
+		[HttpPut]
+		[Route("api/users/{id}")]
+		public User UpdateUser(Guid id, [FromBody] User user)
+		{
+			return null;
+		}
+	}
+}
